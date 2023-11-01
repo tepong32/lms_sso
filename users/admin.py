39f88d7aa8e556cbcf35e6f029a1732c6f37577f
teppy <tepong32@gmail.com>
2023-11-01 23:02:31 +0800

    partial updates/milestones for the project

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+# from .models import User
 
-# Register your models here.
+from django.contrib import admin
+from .models import User
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('staff_id', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
+    
+    fieldsets = (
+        (None, {'fields': ('staff_id', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'middle_name', 'ext_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,13 +1,95 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.utils import timezone
 
-class User(AbstractUser):
-    '''
+
+### Documentation: ###
+# See: https://docs.djangoproject.com/en/4.2/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
+'''
     If you’re starting a new project, it’s highly recommended to set up a custom user model, even if the default User model is sufficient for you.
     This model behaves identically to the default user model, but you’ll be able to customize it in the future if the need arises.
-    See: https://docs.djangoproject.com/en/4.2/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
-
     Configure users.User to be the model used for the auth application by adding AUTH_USER_MODEL to settings.py:
     AUTH_USER_MODEL='users.User'
+'''
+# THIS NEEDS TO BE DONE PRIOR TO THE FIRST DB MIGRATION OR YOUR APP WILL FAIL
+
+
+### for reference, follow-along: https://www.youtube.com/watch?v=mndLkCEiflg
+### NOTE: Almost everything is the same as AbstractUser aside from allowing blank=True on some fields specifically for this LMS we are working on.
+
+class CustomUserManager(UserManager):
     '''
-    pass
+        Our own UserManager that we will use when using the manage.py file
+    '''
+    def _create_user(self, staff_id, password, **extra_fields):
+        if not staff_id:
+            '''check if the user provided a valid email address'''
+            raise ValueError("Please provide a valid Staff ID.")
+
+        # email = self.normalize_email(email)
+        user = self.model(staff_id=staff_id, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    '''function used in cmd to create a regular user'''
+    def create_user(self, staff_id=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(staff_id, password, **extra_fields)
+
+    '''function used in cmd to create a super user'''
+    def create_superuser(self, staff_id=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        # just double-checking the superuser creation meets the required priviledges
+        # __( I asked chatGPT what these two if lines are for. XD )__
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(staff_id, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    '''
+        the default (custom) User model the app will use 
+    '''
+    staff_id = models.PositiveIntegerField(unique=True) # change max_length if needed, add min_length/value
+    email = models.EmailField(blank=True, default="") # add unique & null field options in production!
+    first_name = models.CharField(max_length=50, blank=True)
+    middle_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    ext_name = models.CharField(max_length=3, blank=True)
+    
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = CustomUserManager() # set the CustomUserManager() above instead of default UserManager()
+
+    USERNAME_FIELD = "staff_id" # default is "email" because of what we set on the CustomUserManager()
+    REQUIRED_FIELDS = [] # ["email", "username", "first_name", "last_name"]
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = 'Users'
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name}".strip()
+        elif self.first_name:
+            return self.first_name
+        elif self.last_name:
+            return self.last_name
+        else:
+            return "User has not provided his/her name yet."
+
+    def get_short_name(self):
+        return self.first_name or self.email.split('@')[0] or "User has not provided a name nor email yet."
