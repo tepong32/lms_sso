4fe3a8c718316f9fee7cfbe1fc30776c865662e4
teppy <tepong32@gmail.com>
2023-11-16 09:34:32 +0800

    implemented filters for API display

--- home/views.py
+++ home/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from users.models import User
+from users.models import User, Profile, EmployeeType
 # Create your views here.
 
 def homeView(request):
     user = User
-    context = {}
+    profile = Profile
+    context = {
+        'advisors': Profile.objects.filter(emp_type__name=EmployeeType.Type.ADVISOR),
+        'tls': Profile.objects.filter(emp_type__name=EmployeeType.Type.TEAM_LEADER),
+        'oms': Profile.objects.filter(emp_type__name=EmployeeType.Type.OPERATIONS_MGR),
+    }
     return render(request, 'home/authed/home.html', context)
